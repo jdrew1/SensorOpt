7e0524c9d49115239c170a41ec64829d839a1474
jordan <jordan@jordans-MacBook-Pro.local>
2022-08-12 15:23:33 +0200

    re-organization of modules

---
+++ modules/PythonAPI.cpp
@@ -0,0 +1,53 @@
+#include "PythonAPI.h"
+
+namespace CarlaAPI{
+    void SetupPython(char ** args){
+        Py_Initialize();
+        //format the arguments so that python can interpret them correctly:
+        wchar_t **wargs = (wchar_t**)malloc(sizeof(wchar_t *));
+        size_t * size = (size_t*)malloc(sizeof(size_t));
+        *size = sizeof(**args);
+        *wargs = Py_DecodeLocale(*args, size);
+        //make sure python is able to see the files in the current directory
+        PySys_SetArgv(1, wargs);
+    }
+
+    PyObject* RunPyScript(std::string pyFunctName, std::string pythonArgs, std::string pyScriptLoc){
+
+        MyLogger::SaveToLog(("RunPyScript: Running Python Script: " +
+                             pyFunctName + "(" + pythonArgs + ")").c_str(), MyLogger::Message);
+        PyObject* pName = PyUnicode_FromString(pyScriptLoc.c_str());
+        PyObject* pModule = PyImport_Import(pName);
+
+        if(pModule){
+            PyObject* pFunc = PyObject_GetAttrString(pModule, pyFunctName.c_str());
+            if (!PyCallable_Check(pFunc))
+                MyLogger::SaveToLog(((std::string)"RunPyScript: Py function not callable: " + pyFunctName).c_str(), MyLogger::FATAL);
+            if(pFunc && PyCallable_Check(pFunc)){
+                PyObject* pValue = PyObject_CallFunction(pFunc, "s", pythonArgs.c_str());
+                if (pValue) return pValue;
+                else MyLogger::SaveToLog("RunPyScript: No return value from Python script", MyLogger::Error);
+            }
+            else MyLogger::SaveToLog(("RunPyScript: Python Function not found: " + pyFunctName).c_str(), MyLogger::FATAL);
+        }
+        else MyLogger::SaveToLog(("RunPyScript: Python Module not found: " + pyScriptLoc).c_str(), MyLogger::FATAL);
+        return nullptr;
+    }
+
+    void ParsePyObject(PyObject* toParse){
+        MyLogger::SaveToLog("ParsePyObject: Interpreting Python Object", MyLogger::Message);
+        if (toParse == nullptr) {
+            MyLogger::SaveToLog("ParsePyObject: Python object is null", MyLogger::Error);
+            return;
+        }
+        std::string pyType = toParse->ob_type->tp_name;
+        if (pyType != "list")
+            return;
+        PyObject * firstPyObj = PySequence_GetItem(toParse,0);
+        //printf(PyBytes_AsString(firstPyObj));
+    }
+
+    void ExitPython(){
+        Py_Finalize();
+    }
+}
---
+++ modules/logger.cpp
@@ -0,0 +1,58 @@
+#include "logger.h"
+
+namespace MyLogger{
+    using namespace std;
+    static void InitLogging(){
+        time_t currentTime = time(0);
+        SettingsFile::GetSetting("logfilePath", &logPath);
+        fstream logFile;
+        logFile.open(logPath, ios::in);
+        if (!logFile){
+            logFile.open(logPath, ios::app);
+            logFile << "Created new LogFile: " << ctime(&currentTime) << "----------------------------------------" << endl;
+            std::cout << "Created new LogFile: " << ctime(&currentTime) << "----------------------------------------" << endl;
+        }
+        else{
+            logFile.close();
+            logFile.open(logPath, ios::app);
+            logFile << "New run of program: " << ctime(&currentTime) << "----------------------------------------" << endl;
+            std::cout << "New run of program: " << ctime(&currentTime) << "----------------------------------------" << endl;
+        }
+        logFile.close();
+    }
+    static bool SaveToLog(const char* message, LogType type){
+        ofstream logFile;
+        logFile.open(logPath, ios::app);
+        if (!logFile){
+            cout << "ERROR: LOGFILE CHANGED OR REMOVED!" << endl;
+            return false;
+        }
+        else{
+            string preface;
+            switch(type){
+                case 0:
+                    preface = "Error: \t";
+                    break;
+                case 1:
+                    preface = "Warning: \t";
+                    break;
+                case 2:
+                    preface = "FATAL: \n =============================================== \n\t";
+                    break;
+                case 3:
+                    preface = "IO: \t";
+                    break;
+                case 4:
+                    preface = "Message: \t";
+                    break;
+                case 5:
+                    preface = "Status: \t";
+                    break;
+            }
+            logFile << preface << message << endl;
+            std::cout << preface << message << endl;
+        }
+        logFile.close();
+        return true;
+    }
+}
